50a3c6d86a2c6e9305ffe52595c56b195361991a
Carlos O'Ryan <coryan@google.com>
2026-09-21 20:01:37 -0400

    chore: update librarian with suppressed diagnostics (#1014)
    In this version of librarian all the 'DeprecatedDeclaration' warnings
    are supressed.
    
    We want these diagnostics because they inform customers when they are
    trying to use a service, method, message, field, enum or enum value that
    has been deprecated.
    
    We also need to use the same elements, if nothing else, to return them
    when the service includes them in a response, or allow customers to keep
    using them until they have time to fix their code.
    
    Until Swift 6.4 we could not supress the diagnostics, which was too
    noisy. With this PR the diagnostics are suppressed, and the CI system
    now can enable warnings for other locations (say examples) in case we
    use the deprecated elements where we shouldn't.
    
    On Swift 6.2 and 6.3, the diagnostics are not suppressed. Not ideal. On
    the other hand, we expect those to go away in 18 months or so.

--- Sources/GoogleCloudIAPV1/ReauthSettings.swift
+++ Sources/GoogleCloudIAPV1/ReauthSettings.swift
@@ -157,6 +157,9 @@ public struct ReauthSettings: Codable, Equatable, GoogleWKT._AnyPackable,
     /// Initialize from a string value.
     ///
     /// If the value is unknown, this initializes to [`unknownStringValue`](doc:Method/unknownStringValue(_:)).
+    #if hasAttribute(diagnose)
+      @diagnose(DeprecatedDeclaration, as: ignored)
+    #endif
     public init(stringValue: Swift.String) {
       switch stringValue {
       case "METHOD_UNSPECIFIED": self = .unspecified
@@ -171,6 +174,9 @@ public struct ReauthSettings: Codable, Equatable, GoogleWKT._AnyPackable,
     /// Initialize from an integer value.
     ///
     /// If the value is unknown, this initializes to [`unknownIntValue`](doc:Method/unknownIntValue(_:)).
+    #if hasAttribute(diagnose)
+      @diagnose(DeprecatedDeclaration, as: ignored)
+    #endif
     public init(intValue: Int) {
       switch intValue {
       case 0: self = .unspecified
