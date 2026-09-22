69137458a09cfa3c5c1dabb0e49b4a365f205f14
Carlos O'Ryan <coryan@google.com>
2026-09-21 20:42:28 -0400

    chore: update librarian w/ better enum docs (#1017)
    In this version of librarian we improve the generated documentation for
    `enum` so the possible evolution and usage of the enums is more obvious.
    For example, we recommend using `@unknown default:` so they get warnings
    when new values appear in the enum.

--- Sources/GoogleCloudIAPV1/AttributePropagationSettings.swift
+++ Sources/GoogleCloudIAPV1/AttributePropagationSettings.swift
@@ -125,6 +125,12 @@ public struct AttributePropagationSettings: Codable, Equatable, GoogleWKT._AnyPa
   /// Supported output credentials for attribute propagation. Each output
   /// credential maps to a "field" in the response. For example, selecting JWT
   /// will propagate all attributes in the IAP JWT, header in the headers, etc.
+  ///
+  /// - Note: Adding cases to this enumeration is not considered a breaking change.
+  ///   Always include an `@unknown default:` case when switching over this type.
+  ///   Do not pattern-match against `unknownStringValue` or `unknownIntValue`
+  ///   expecting specific values to remain unparsed; future releases may promote
+  ///   them to named cases.
   public enum OutputCredentials: Codable, Equatable, Sendable {
     /// An output credential is required.
     case unspecified
@@ -138,15 +144,21 @@ public struct AttributePropagationSettings: Codable, Equatable, GoogleWKT._AnyPa
     case rctoken
     /// Encodes an unknown integer value.
     ///
-    /// The most common cause for an unknown values is for the service to send
+    /// The most common cause for an unknown value is for the service to send
     /// a value unknown to the library. We recommend you update your library to
     /// the latest version.
+    ///
+    /// - Warning: Do not pattern-match specific integer values in this case;
+    ///   future releases may promote them to named enum cases.
     case unknownIntValue(Int)
     /// Encodes an unknown string value.
     ///
-    /// The most common cause for an unknown values is for the service to send
+    /// The most common cause for an unknown value is for the service to send
     /// a value unknown to the library. We recommend you update your library to
     /// the latest version.
+    ///
+    /// - Warning: Do not pattern-match specific string literals in this case;
+    ///   future releases may promote them to named enum cases.
     case unknownStringValue(String)
 
     public init() {
